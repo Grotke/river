296e886d4bcae0ddc2b581a1359b3f3472478a86
Joseph Montgomery <joseph.conan.montgomery@gmail.com>
2023-03-18 15:12:57 -0700

    Remove more unused code. Do some clean up and commenting

--- final_project.cpp
+++ final_project.cpp
@@ -1,7 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <time.h>
+#include <ctime>
 #define _USE_MATH_DEFINES
 #include <math.h>
 
@@ -18,79 +15,61 @@
 #include <vector>
 #include "utils.h"
 
-
-//	This is a sample OpenGL / GLUT program
-//
-//	The objective is to draw a 3d object and change the color of the axes
-//		with a glut menu
-//
 //	The left mouse button does rotation
 //	The middle mouse button does scaling
 //	The user interface allows:
 //		1. The axes to be turned on and off
-//		2. The color of the axes to be changed
 //		3. Debugging to be turned on and off
-//		4. Depth cueing to be turned on and off
 //		5. The projection to be changed
 //		6. The transformations to be reset
 //		7. The program to quit
 //
 //	Author:			Joseph Montgomery
 
-// NOTE: There are a lot of good reasons to use const variables instead
-// of #define's.  However, Visual C++ does not allow a const variable
-// to be used as an array size or as the case in a switch( ) statement.  So in
-// the following, all constants are const variables except those which need to
-// be array sizes or cases in switch( ) statements.  Those are #defines.
+// Window titles
+constexpr char* WINDOWTITLE{ "Final Project: River Animated with Flow Tiles -- Joseph Montgomery" };
+constexpr char* GLUITITLE{ "User Interface Window" };
 
-// title of these windows:
-
-const char* WINDOWTITLE = { "Final Project: River Animated with Flow Tiles -- Joseph Montgomery" };
-const char* GLUITITLE = { "User Interface Window" };
-
+// Shader helper class
 GLSLProgram* Pattern;
 
 // what the glui package defines as true and false:
 
-const int GLUITRUE = { true };
-const int GLUIFALSE = { false };
+constexpr int GLUITRUE{ true };
+constexpr int GLUIFALSE{ false };
 
-// the escape key:
+// Escape key
 
-#define ESCAPE		0x1b
+constexpr int ESCAPE = 0x1b;
 
 // initial window size:
 
-const int INIT_WINDOW_SIZE = { 1200 };
-
-// size of the 3d box:
-
-const float BOXSIZE = { 2.f };
+constexpr int INIT_WINDOW_SIZE{ 1200 };
 
 // multiplication factors for input interaction:
 //  (these are known from previous experience)
 
-const float ANGFACT = { 1. };
-const float SCLFACT = { 0.005f };
+constexpr float ANGFACT{ 1. };
+constexpr float SCLFACT{ 0.005f };
 
 // minimum allowable scale factor:
 
-const float MINSCALE = { 0.05f };
+constexpr float MINSCALE{ 0.05f };
 
 // scroll wheel button values:
 
-const int SCROLL_WHEEL_UP = { 3 };
-const int SCROLL_WHEEL_DOWN = { 4 };
+constexpr int SCROLL_WHEEL_UP{ 3 };
+constexpr int SCROLL_WHEEL_DOWN{ 4 };
 
 // equivalent mouse movement when we click a the scroll wheel:
 
-const float SCROLL_WHEEL_CLICK_FACTOR = { 5. };
+constexpr float SCROLL_WHEEL_CLICK_FACTOR{ 5. };
 
 // active mouse buttons (or them together):
 
-const int LEFT = { 4 };
-const int MIDDLE = { 2 };
-const int RIGHT = { 1 };
+constexpr int LEFT{ 4 };
+constexpr int MIDDLE{ 2 };
+constexpr int RIGHT{ 1 };
 
 // which projection:
 
@@ -110,75 +89,11 @@ enum ButtonVals
 
 // window background color (rgba):
 
-const GLfloat BACKCOLOR[] = { 0., 0., 0., 1. };
+const GLfloat BACKGROUND_COLOR[] = { 0., 0., 0., 1. };
 
 // line width for the axes:
 
-const GLfloat AXES_WIDTH = { 3. };
-
-// the color numbers:
-// this order must match the radio button order
-
-enum Colors
-{
-	RED,
-	YELLOW,
-	GREEN,
-	CYAN,
-	BLUE,
-	MAGENTA,
-	WHITE,
-	BLACK
-};
-
-char* ColorNames[] =
-{
-	"Red",
-	"Yellow",
-	"Green",
-	"Cyan",
-	"Blue",
-	"Magenta",
-	"White",
-	"Black"
-};
-
-// the color definitions:
-// this order must match the menu order
-
-const GLfloat Colors[][3] =
-{
-	{ 1., 0., 0. },		// red
-	{ 1., 1., 0. },		// yellow
-	{ 0., 1., 0. },		// green
-	{ 0., 1., 1. },		// cyan
-	{ 0., 0., 1. },		// blue
-	{ 1., 0., 1. },		// magenta
-	{ 1., 1., 1. },		// white
-	{ 0., 0., 0. },		// black
-};
-
-// fog parameters:
-
-const GLfloat FOGCOLOR[4] = { .0, .0, .0, 1. };
-const GLenum  FOGMODE = { GL_LINEAR };
-const GLfloat FOGDENSITY = { 0.30f };
-const GLfloat FOGSTART = { 1.5 };
-const GLfloat FOGEND = { 4. };
-
-
-// what options should we compile-in?
-// in general, you don't need to worry about these
-// i compile these in to show class examples of things going wrong
-
-//#define DEMO_Z_FIGHTING
-//#define DEMO_DEPTH_BUFFER
-
-// should we turn the shadows on?
-
-//#define ENABLE_SHADOWS
-
-
+constexpr GLfloat AXES_WIDTH{ 3. };
 
 // non-constant global variables:
 
@@ -186,41 +101,16 @@ int		ActiveButton;			// current button that is down
 GLuint	AxesList;				// list to hold the axes
 int		AxesOn;					// != 0 means to draw the axes
 int		DebugOn;				// != 0 means to print debugging info
-int		DepthCueOn;				// != 0 means to use intensity depth cueing
-int		DepthBufferOn;			// != 0 means to use the z-buffer
-int		DepthFightingOn;		// != 0 means to force the creation of z-fighting
-GLuint	BoxList;				// object display list
-GLuint waveList;
 
 int		MainWindow;				// window id for main graphics window
 float	Scale;					// scaling factor
-int		ShadowsOn;				// != 0 means to turn shadows on
-int		WireframeOn;
 int		WhichColor;				// index into Colors[ ]
 int		WhichProjection;		// ORTHO or PERSP
 int		Xmouse, Ymouse;			// mouse values
 float	Xrot, Yrot;				// rotation angles in degrees
-float	groundOffset = -0.5f;
-float	groundLength = 20.0f;
-float	groundWidth = 10.0f;
-const int TRIANGLES_ACROSS = 100;
-const int TRIANGLES_LONG = 200;
 float Time;
-bool Distort;
-bool TextureOn;
-bool AnimateFrag;
-bool AnimateVert;
-unsigned char* Texture;
-int width, height;
-const float SPHERE_RADIUS = 2.0f;
-const int SLICES = 100;
-const int STACKS = 200;
-float vertAnimate = 0;
-float fragAnimate = 0;
 
-constexpr int NUM_CURVES = 10;
-bool DisplayCurves;
-bool DisplayPoints;
+// River globals
 GLuint TerrainList;
 GLuint TerrainTexture, WaterTexture, WaterNormalMap, FlowMap, RiverMap;
 const float BLOCKS = 16.f;
@@ -232,21 +122,7 @@ bool UseEdgeTransparancy;
 bool ShowWater;
 bool ShinyWater;
 
-struct Point
-{
-	float x0, y0, z0;       // initial coordinates
-	float x, y, z;        // animated coordinates
-};
-
-struct Curve
-{
-	float r, g, b;
-	Point* points;
-	int count;
-};
-
-Curve Curves[NUM_CURVES];
-#define MS_IN_THE_ANIMATION_CYCLE	10000
+constexpr int MS_IN_THE_ANIMATION_CYCLE = 10000;
 
 
 // function prototypes:
@@ -254,17 +130,9 @@ Curve Curves[NUM_CURVES];
 void	Animate();
 void	Display();
 void	DoAxesMenu(int);
-void	DoColorMenu(int);
-void	DoDepthBufferMenu(int);
-void	DoDepthFightingMenu(int);
-void	DoDepthMenu(int);
 void	DoDebugMenu(int);
 void	DoMainMenu(int);
-void	ToggleLight(int);
-void	DoProjectMenu(int);
-void	DoTextureMenu(int);
-void	ToggleWireframe(int);
-void	DoShadowMenu();
+void	DoProjectionMenu(int);
 float	ElapsedSeconds();
 void	InitGraphics();
 void	InitLists();
@@ -275,20 +143,8 @@ void	MouseMotion(int, int);
 void	Reset();
 void	Resize(int, int);
 void	Visibility(int);
-struct point {
-	float x, y, z;		// coordinates
-	float nx, ny, nz;	// surface normal
-	float s, t;		// texture coords
-};
-
-float White[] = { 1.,1.,1.,1. };
-// utility to createan array from 3 separatevalues:
-float *Array3( float a, float b, float c){static float array[4];array[0] =a;array[1] =b;array[2] =c;array[3] =1.;return array;}
-// utility to createan array from a multiplier and an array:
-float* MulArray3(float factor, float array0[3]) { static float array[4]; array[0] = factor * array0[0]; array[1] = factor * array0[1]; array[2] = factor * array0[2]; array[3] = 1.; return array; }
 
 // main program:
-
 int
 main(int argc, char* argv[])
 {
@@ -374,10 +230,6 @@ Display()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glEnable(GL_DEPTH_TEST);
-#ifdef DEMO_DEPTH_BUFFER
-	if (DepthBufferOn == 0)
-		glDisable(GL_DEPTH_TEST);
-#endif
 
 
 	// specify shading to be flat:
@@ -430,32 +282,17 @@ Display()
 			Scale = MINSCALE;
 		glScalef((GLfloat)Scale, (GLfloat)Scale, (GLfloat)Scale);
 
-	// set the fog parameters:
-
-	if (DepthCueOn != 0)
-	{
-		glFogi(GL_FOG_MODE, FOGMODE);
-		glFogfv(GL_FOG_COLOR, FOGCOLOR);
-		glFogf(GL_FOG_DENSITY, FOGDENSITY);
-		glFogf(GL_FOG_START, FOGSTART);
-		glFogf(GL_FOG_END, FOGEND);
-		glEnable(GL_FOG);
-	}
-	else
-	{
-		glDisable(GL_FOG);
-	}
-
 
 	// possibly draw the axes:
 
 	if (AxesOn != 0)
 	{
-		glColor3fv(&Colors[WhichColor][0]);
+		GLfloat white[3]{ 1., 1., 1. };
+		glColor3fv(white);
 		glCallList(AxesList);
 	}
-	//glEnable(GL_TEXTURE_2D);
 
+	// Activate shader and set up uniforms
 	Pattern->Use();
 	//uniform float uKa, uKd, uKs; // coefficients of each type of lighting
 	//uniform vec3 uColor;		// object color
@@ -484,6 +321,7 @@ Display()
 		Pattern->SetUniformVariable("uTime", 0);
 	}
 
+	// Set up river textures/maps
 	glActiveTexture( GL_TEXTURE0 );
 	glBindTexture(GL_TEXTURE_2D, TerrainTexture);
 	Pattern->SetUniformVariable("uTerrainTexUnit", 0 );
@@ -491,7 +329,6 @@ Display()
 	glActiveTexture(GL_TEXTURE1);
 	glBindTexture(GL_TEXTURE_2D, RiverMap);
 	Pattern->SetUniformVariable("uRiverMapTexUnit", 1);
-	//GLuint TerrainTexture, WaterTexture, WaterNormalMap, FlowMap, RiverMap;
 
 	glActiveTexture(GL_TEXTURE2);
 	glBindTexture(GL_TEXTURE_2D, WaterNormalMap);
@@ -505,48 +342,14 @@ Display()
 	glBindTexture(GL_TEXTURE_2D, FlowMap);
 	Pattern->SetUniformVariable("uFlowMapTexUnit", 4);
 
-	//glEnable(GL_TEXTURE_2D);
+	// Scale down model since it's pretty big for camera view
 	glPushMatrix();
 	glScalef(0.3, 0.3, 0.3);
 	glCallList(TerrainList);
 	glPopMatrix();
-	//glDisable(GL_TEXTURE_2D);
 
+	// Turn off shader
 	Pattern->Use(0);
-
-#ifdef DEMO_Z_FIGHTING
-	if (DepthFightingOn != 0)
-	{
-		glPushMatrix();
-		glRotatef(90., 0., 1., 0.);
-		glCallList(BoxList);
-		glPopMatrix();
-	}
-#endif
-
-
-	// draw some gratuitous text that just rotates on top of the scene:
-
-	glDisable(GL_DEPTH_TEST);
-	glColor3f(0., 1., 1.);
-
-	// draw some gratuitous text that is fixed on the screen:
-	//
-	// the projection matrix is reset to define a scene whose
-	// world coordinate system goes from 0-100 in each axis
-	//
-	// this is called "percent units", and is just a convenience
-	//
-	// the modelview matrix is reset to identity as we don't
-	// want to transform these coordinates
-
-	glDisable(GL_DEPTH_TEST);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(0., 100., 0., 100.);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	glColor3f(1., 1., 1.);
 
 	// swap the double-buffered framebuffers:
 
@@ -569,16 +372,6 @@ DoAxesMenu(int id)
 	glutPostRedisplay();
 }
 
-
-void
-DoColorMenu(int id)
-{
-	gluLookAt(0., 0., 3., 0., 0., 0., 0., 1., 0.);
-	glutSetWindow(MainWindow);
-	glutPostRedisplay();
-}
-
-
 void
 DoDebugMenu(int id)
 {
@@ -587,37 +380,6 @@ DoDebugMenu(int id)
 	glutSetWindow(MainWindow);
 	glutPostRedisplay();
 }
-
-
-void
-DoDepthBufferMenu(int id)
-{
-	DepthBufferOn = id;
-
-	glutSetWindow(MainWindow);
-	glutPostRedisplay();
-}
-
-
-void
-DoDepthFightingMenu(int id)
-{
-	DepthFightingOn = id;
-
-	glutSetWindow(MainWindow);
-	glutPostRedisplay();
-}
-
-
-void
-DoDepthMenu(int id)
-{
-	DepthCueOn = id;
-
-	glutSetWindow(MainWindow);
-	glutPostRedisplay();
-}
-
 
 // main menu callback:
 
@@ -648,33 +410,8 @@ DoMainMenu(int id)
 	glutPostRedisplay();
 }
 
-void ToggleLight(int light)
-{
-	if (light == 0) {
-		glEnable(GL_LIGHT0);
-		glDisable(GL_LIGHT1);
-		glDisable(GL_LIGHT2);
-	}
-	else if (light == 1) {
-		glEnable(GL_LIGHT1);
-		glDisable(GL_LIGHT0);
-		glDisable(GL_LIGHT2);
-	}
-	else if (light == 2) {
-		glEnable(GL_LIGHT2);
-		glDisable(GL_LIGHT0);
-		glDisable(GL_LIGHT1);
-	}
-	else {
-		glEnable(GL_LIGHT0);
-		glEnable(GL_LIGHT1);
-		glEnable(GL_LIGHT2);
-	}
-}
-
-
 void
-DoProjectMenu(int id)
+DoProjectionMenu(int id)
 {
 	WhichProjection = id;
 
@@ -682,45 +419,7 @@ DoProjectMenu(int id)
 	glutPostRedisplay();
 }
 
-void
-DoTextureMenu(int id)
-{
-	if (id == 0) {
-		TextureOn = false;
-		Distort = false;
-	}
-	else if (id == 1) {
-		TextureOn = true;
-		Distort = false;
-	}
-	else if (id == 2) {
-		TextureOn = true;
-		Distort = true;
-	}
-	glutSetWindow(MainWindow);
-	glutPostRedisplay();
-}
-
-void ToggleWireframe(int id)
-{
-	WireframeOn = !WireframeOn;
-	glutSetWindow(MainWindow);
-	glutPostRedisplay();
-}
-
-
-
-void
-DoShadowsMenu(int id)
-{
-	ShadowsOn = id;
-
-	glutSetWindow(MainWindow);
-	glutPostRedisplay();
-}
-
 // return the number of seconds since the start of the program:
-
 float
 ElapsedSeconds()
 {
@@ -741,26 +440,7 @@ InitMenus()
 {
 	glutSetWindow(MainWindow);
 
-	int numColors = sizeof(Colors) / (3 * sizeof(int));
-	int colormenu = glutCreateMenu(DoColorMenu);
-	for (int i = 0; i < numColors; i++)
-	{
-		glutAddMenuEntry(ColorNames[i], i);
-	}
-
 	int axesmenu = glutCreateMenu(DoAxesMenu);
-	glutAddMenuEntry("Off", 0);
-	glutAddMenuEntry("On", 1);
-
-	int depthcuemenu = glutCreateMenu(DoDepthMenu);
-	glutAddMenuEntry("Off", 0);
-	glutAddMenuEntry("On", 1);
-
-	int depthbuffermenu = glutCreateMenu(DoDepthBufferMenu);
-	glutAddMenuEntry("Off", 0);
-	glutAddMenuEntry("On", 1);
-
-	int depthfightingmenu = glutCreateMenu(DoDepthFightingMenu);
 	glutAddMenuEntry("Off", 0);
 	glutAddMenuEntry("On", 1);
 
@@ -768,39 +448,14 @@ InitMenus()
 	glutAddMenuEntry("Off", 0);
 	glutAddMenuEntry("On", 1);
 
-	int projmenu = glutCreateMenu(DoProjectMenu);
+	int projmenu = glutCreateMenu(DoProjectionMenu);
 	glutAddMenuEntry("Orthographic", ORTHO);
 	glutAddMenuEntry("Perspective", PERSP);
 
-	int shadowsmenu = glutCreateMenu(DoShadowsMenu);
-	glutAddMenuEntry("Off", 0);
-	glutAddMenuEntry("On", 1);
-
-	int texturemenu = glutCreateMenu(DoTextureMenu);
-	glutAddMenuEntry("None", 0);
-	glutAddMenuEntry("Normal Texture", 1);
-	glutAddMenuEntry("Distorted Texture", 2);
-
 	int mainmenu = glutCreateMenu(DoMainMenu);
 	glutAddSubMenu("Axes", axesmenu);
-	glutAddSubMenu("Colors", colormenu);
-	glutAddSubMenu("Textures", texturemenu);
 
-
-#ifdef DEMO_DEPTH_BUFFER
-	glutAddSubMenu("Depth Buffer", depthbuffermenu);
-#endif
-
-#ifdef DEMO_Z_FIGHTING
-	glutAddSubMenu("Depth Fighting", depthfightingmenu);
-#endif
-
-	glutAddSubMenu("Depth Cue", depthcuemenu);
 	glutAddSubMenu("Projection", projmenu);
-
-#ifdef ENABLE_SHADOWS
-	glutAddSubMenu("Shadows", shadowsmenu);
-#endif
 
 	glutAddMenuEntry("Reset", RESET);
 	glutAddSubMenu("Debug", debugmenu);
@@ -836,7 +491,7 @@ InitGraphics()
 
 	// set the framebuffer clear values:
 
-	glClearColor(BACKCOLOR[0], BACKCOLOR[1], BACKCOLOR[2], BACKCOLOR[3]);
+	glClearColor(BACKGROUND_COLOR[0], BACKGROUND_COLOR[1], BACKGROUND_COLOR[2], BACKGROUND_COLOR[3]);
 
 	// setup the callback functions:
 	// DisplayFunc -- redraw the window
@@ -844,19 +499,8 @@ InitGraphics()
 	// KeyboardFunc -- handle a keyboard input
 	// MouseFunc -- handle the mouse button going down or up
 	// MotionFunc -- handle the mouse moving with a button down
-	// PassiveMotionFunc -- handle the mouse moving with a button up
 	// VisibilityFunc -- handle a change in window visibility
-	// EntryFunc	-- handle the cursor entering or leaving the window
-	// SpecialFunc -- handle special keys on the keyboard
-	// SpaceballMotionFunc -- handle spaceball translation
-	// SpaceballRotateFunc -- handle spaceball rotation
-	// SpaceballButtonFunc -- handle spaceball button hits
-	// ButtonBoxFunc -- handle button box hits
-	// DialsFunc -- handle dial rotations
-	// TabletMotionFunc -- handle digitizing tablet motion
-	// TabletButtonFunc -- handle digitizing tablet button hits
 	// MenuStateFunc -- declare when a pop-up menu is in use
-	// TimerFunc -- trigger something to happen a certain time from now
 	// IdleFunc -- what to do when nothing else is going on
 
 	glutSetWindow(MainWindow);
@@ -865,23 +509,12 @@ InitGraphics()
 	glutKeyboardFunc(Keyboard);
 	glutMouseFunc(MouseButton);
 	glutMotionFunc(MouseMotion);
-	glutPassiveMotionFunc(NULL);
 	glutVisibilityFunc(Visibility);
-	glutEntryFunc(NULL);
-	glutSpecialFunc(NULL);
-	glutSpaceballMotionFunc(NULL);
-	glutSpaceballRotateFunc(NULL);
-	glutSpaceballButtonFunc(NULL);
-	glutButtonBoxFunc(NULL);
-	glutDialsFunc(NULL);
-	glutTabletMotionFunc(NULL);
-	glutTabletButtonFunc(NULL);
-	glutMenuStateFunc(NULL);
-	glutTimerFunc(-1, NULL, 0);
 	glutIdleFunc(Animate);
 
 	srand(time(0));
 
+	// Set up textures
 	unsigned char* TextureArrayTerrain = BmpToTexture("final_project_assets/final_terrain_texture_v2_revised_banks.bmp", &totalTerrainWidth, &totalTerrainHeight);
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 	glGenTextures(1, &TerrainTexture);
@@ -939,8 +572,6 @@ InitGraphics()
 	glTexImage2D(GL_TEXTURE_2D, 0, 3, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, TextureArrayRiverMap);
 
 
-	//WaterTexture, WaterNormalMap, FlowMap
-
 	// init glew (a window must be open to do this):
 
 #ifdef WIN32
@@ -954,6 +585,7 @@ InitGraphics()
 	fprintf(stderr, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
 #endif
 
+	// Create shaders
 	Pattern = new GLSLProgram();
 	bool valid = Pattern->Create("final_project_assets/river.vert", "final_project_assets/river.frag");
 
@@ -971,32 +603,7 @@ InitGraphics()
 void
 InitLists()
 {
-	float dx = BOXSIZE / 2.f;
-	float dy = BOXSIZE / 2.f;
-	float dz = BOXSIZE / 2.f;
 	glutSetWindow(MainWindow);
-
-	waveList = glGenLists(1);
-	glNewList(waveList, GL_COMPILE);
-	glBegin(GL_LINE_STRIP);
-	float widthPerTriangle = groundWidth / TRIANGLES_ACROSS;
-	float lengthPerTriangle = groundLength / TRIANGLES_LONG;
-	float triangleZEdge = groundWidth / 2.0f;
-	float triangleXEdge = -groundLength / 2.0f;
-	for (int i = 0; i < TRIANGLES_LONG; i++) {
-		glNormal3f(0.0, 1.0, 0.0);
-		glVertex3f(triangleXEdge + lengthPerTriangle, groundOffset, triangleZEdge);
-		glVertex3f(triangleXEdge, groundOffset, triangleZEdge);
-		for (int j = 0; j < TRIANGLES_ACROSS; j++) {
-			glVertex3f(triangleXEdge + lengthPerTriangle, groundOffset, triangleZEdge - widthPerTriangle);
-			glVertex3f(triangleXEdge, groundOffset, triangleZEdge - widthPerTriangle);
-			triangleZEdge -= widthPerTriangle;
-		}
-		triangleZEdge = groundWidth / 2.0f;
-		triangleXEdge += lengthPerTriangle;
-	}
-	glEnd();
-	glEndList();
 
 	// create the axes:
 	AxesList = glGenLists(1);
@@ -1006,6 +613,7 @@ InitLists()
 	glLineWidth(1.);
 	glEndList();
 
+	// Create riverbed model
 	TerrainList = glGenLists(1);
 	glNewList(TerrainList, GL_COMPILE);
 	glPushMatrix();
@@ -1040,19 +648,6 @@ Keyboard(unsigned char c, int x, int y)
 	case ESCAPE:
 		DoMainMenu(QUIT);	// will not return here
 		break;				// happy compiler
-
-	case '0':
-		ToggleLight(0);
-		break;
-	case '1':
-		ToggleLight(1);
-		break;
-	case '2':
-		ToggleLight(2);
-		break;
-	case '3':
-		ToggleLight(3);
-		break;
 
 	case 't':
 		UseTransparency = !UseTransparency;
@@ -1188,21 +783,9 @@ Reset()
 	ActiveButton = 0;
 	AxesOn = 1;
 	DebugOn = 0;
-	DepthBufferOn = 1;
-	DepthFightingOn = 0;
-	DepthCueOn = 0;
 	Scale = 1.0;
-	ShadowsOn = 0;
-	WireframeOn = 0;
-	WhichColor = WHITE;
 	WhichProjection = PERSP;
 	Xrot = Yrot = 0.;
-	Distort = false;
-	TextureOn = false;
-	AnimateVert = false;
-	AnimateFrag = false;
-	DisplayPoints = true;
-	DisplayCurves = true;
 	UseTransparency = true;
 	AnimateWater = true;
 	UseEdgeTransparancy = true;
