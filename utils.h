3bde1c707186547ec48bce8568903a506e558202
Joseph Montgomery <joseph.conan.montgomery@gmail.com>
2023-04-10 23:35:39 -0700

    Clean up formatting for utils header

--- utils.h
+++ utils.h
@@ -2,15 +2,14 @@
 #include <stdio.h>
 
 unsigned char* BmpToTexture(char*, int*, int*);
-int				ReadInt(FILE*);
-short			ReadShort(FILE*);
+int ReadInt(FILE*);
+short ReadShort(FILE*);
 
-void			Cross(float[3], float[3], float[3]);
-float			Dot(float[3], float[3]);
-float			Unit(float[3], float[3]);
+void Cross(float[3], float[3], float[3]);
+float Dot(float[3], float[3]);
+float Unit(float[3], float[3]);
 char* ReadRestOfLine(FILE*);
-void	ReadObjVTN(char*, int*, int*, int*);
-float	Unit(float[3]);
-int
-LoadObjFile(char* name);
-void			Axes(float);
+void ReadObjVTN(char*, int*, int*, int*);
+float Unit(float[3]);
+int LoadObjFile(char* name);
+void Axes(float);
